6d07da939c0a7d2b120f4e750c3e7dd7742edd26
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-06-13 17:01:52 +0100

    .

--- soapui-tests/run-test.bat
+++ soapui-tests/run-test.bat
@@ -3,6 +3,7 @@ REM Variable definition
 SET soapuiHome="C:\Program Files\SmartBear\ReadyAPI-2.0.0\"
 SET soapuiProject="%WORKSPACE%\soapui-tests\WebGoat-soapui-project.xml"
 
+
 REM Command line to run the specified tests
 %soapuiHome%\bin\testrunner.bat %soapuiProject% -a -A -f%WORKSPACE%\soapui-tests\_test-reports\%BUILD_TIMESTAMP%\ -j -J -r -a -GbaseURL=http://192.168.0.254:8181 
 
