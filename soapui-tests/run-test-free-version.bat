4e1f3565e451ec37369ec8f08e43a59dd4ec35da
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-06-16 15:40:26 +0100

    .

---
+++ soapui-tests/run-test-free-version.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Variable definition
+SET soapuiHome="C:\Program Files (x86)\SmartBear\SoapUI-5.3.0\"
+SET soapuiProject="%WORKSPACE%\soapui-tests\WebGoat-soapui-project-free-version.xml"
+
+
+REM Command line to run the specified tests
+%soapuiHome%\bin\testrunner.bat -s"TestSuite 1" -r -a -J -f%WORKSPACE%\soapui-tests\_test-reports\ %soapuiProject%
+
+
+
+
+
+
+
