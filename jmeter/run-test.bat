b3592031023f6400bf1168fc18b405f9053c0442
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-07-15 00:00:07 +0100

    .

--- jmeter/run-test.bat
+++ jmeter/run-test.bat
@@ -1,2 +1,2 @@
 set arg1=%1
-apache-jmeter-3.2\bin\jmeter.bat -n -t test-jmeter.jmx -l perf-log.csv -e -o %arg1%\
+jmeter\apache-jmeter-3.2\bin\jmeter.bat -n -t test-jmeter.jmx -l perf-log.csv -e -o %arg1%\
