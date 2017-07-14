f6f39c2bc7fd3eca27ca5fea5b7fa175b11fe3fc
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-07-14 23:18:02 +0100

    .

--- jmeter/jmeter-dashboard-generator.sh
+++ jmeter/jmeter-dashboard-generator.sh
@@ -5,8 +5,7 @@ jenkinsBuildTimestamp="$2"
 
 cd $PWD
 
-sudo -S <<< "cd" bzt $PWD/run-jmeter-test.yml
-
+bzt run-jmeter-test.yml
 
 jmeterLog=$(ls -Rladt -1 $PWD/**/* | grep kpi.jtl | awk 'NR==1{print $9}')
 
