f3f872599daccbd095a367c61f94360cda0bc8e2
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-07-15 00:22:16 +0100

    .

--- jmeter/run-test.bat
+++ jmeter/run-test.bat
@@ -2,4 +2,4 @@ set arg1=%1
 set mypath=%cd%
 
 mkdir %mypath%\%arg1%\
-%mypath%\apache-jmeter-3.2\bin\jmeter.bat -n -t %mypath%\test-jmeter.jmx -l %mypath%\perf-log.csv -e -o %mypath%\%arg1%\
+%mypath%\jmeter\apache-jmeter-3.2\bin\jmeter.bat -n -t %mypath%\jmeter\test-jmeter.jmx -l %mypath%\jmeter\perf-log.csv -e -o %mypath%\jmeter\%arg1%\
