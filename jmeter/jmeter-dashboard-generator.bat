ae23653fa5bead7315abc502a63934ef90a374c7
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-07-17 12:10:29 +0100

    .

--- jmeter/jmeter-dashboard-generator.bat
+++ jmeter/jmeter-dashboard-generator.bat
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-jenkinsWorskspace="$1"
-jenkinsBuildTimestamp="$2"
+FOR /d %%a in (2017*) DO (
+  c:\Users\tjrodrigues.CRITICAL\.bzt\jmeter-taurus\3.2\bin\jmeter -g %%a\kpi.jtl -o D:\jenkins\jenkinsTestSlave\workspace\ContinuousTesting\PerformanceTests\jmeter\_html-reports
 
-cd $PWD
-
-jmeterLog=$(ls -Rladt -1 $PWD/**/* | grep kpi.jtl | awk 'NR==1{print $9}')
-
-mkdir -p _htmlReports/${jenkinsBuildTimestamp}
-sudo -S <<< "cd" ~/.bzt/jmeter-taurus/bin/jmeter -g ${jmeterLog} -o _htmlReports/${jenkinsBuildTimestamp}
-
-
-
+)
