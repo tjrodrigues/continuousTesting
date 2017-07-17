7280768264bd191cb823986ecbd1bd8449dcfb35
Tiago Neto Rodrigues <tjrodrigues@criticalsoftware.com>
2017-07-17 14:01:48 +0100

    .

--- jmeter/jmeter-dashboard-generator.bat
+++ jmeter/jmeter-dashboard-generator.bat
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 FOR /d %%a in (2017*) DO (
   c:\Users\tjrodrigues.CRITICAL\.bzt\jmeter-taurus\3.2\bin\jmeter -g %%a\kpi.jtl -o D:\jenkins\jenkinsTestSlave\workspace\ContinuousTesting\PerformanceTests\jmeter\_html-reports
